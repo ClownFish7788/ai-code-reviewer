39aeea26fb697ab62714cf634625e0a9471063df
ClownFish7788 <3313125840@qq.com>
2026-06-17 17:01:41 +0800

    fix: 修复CodeInput高度塌陷以及滚动条消失bug page 给父元素增加display: flex”

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
             onSubmit={handleSubmit}
             className="flex flex-col flex-1 min-h-0"
           >
-            <div className="relative flex-1 min-h-0">
+            <div className="relative flex flex-col flex-1 min-h-0">
               <CodeInput
                 value={code}
                 onChange={setCode}
